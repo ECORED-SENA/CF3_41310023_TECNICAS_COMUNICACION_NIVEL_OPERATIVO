d68c83b770cf2f14f75b55b6d864b770e7c6ba3d
emilydev <98632409+emilydev@users.noreply.github.com>
2024-09-15 18:39:10 -0500

    Se carga actividad didactica.

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -15,7 +15,7 @@ export default {
   components: { Actividad },
   data: () => ({
     cuestionario: {
-      tema: 'Manejo de los residuos sólidos en la producción avícola.',
+      tema: 'Normatividad empresarial.',
       titulo: 'Cuestionario',
       introduccion:
         'Esta actividad le permitirá determinar el grado de apropiación de los contenidos del componente formativo: <strong>Normatividad empresarial</strong>. Lea cada enunciado y luego seleccione la respuesta correcta según corresponda.',
@@ -27,10 +27,27 @@ export default {
           imagen: require('@/assets/actividad/1.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: true },
-            { id: 'b', texto: '', esCorrecta: false },
-            { id: 'c', texto: '', esCorrecta: false },
-            { id: 'd', texto: '', esCorrecta: false },
+            {
+              id:
+                'Una ley o regla establecida para ser cumplida por un sujeto específico.',
+              texto: '',
+              esCorrecta: true,
+            },
+            {
+              id: 'Un conjunto de principios morales.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id: 'Un código de conducta personal.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id: 'Una recomendación para el comportamiento social.',
+              texto: '',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -42,10 +59,10 @@ export default {
           imagen: require('@/assets/actividad/2.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: true },
-            { id: 'b', texto: '', esCorrecta: false },
-            { id: 'c', texto: '', esCorrecta: false },
-            { id: 'd', texto: '', esCorrecta: false },
+            { id: 'Normas jurídicas.', texto: '', esCorrecta: true },
+            { id: 'Normas educativas.', texto: '', esCorrecta: false },
+            { id: 'Normas informales.', texto: '', esCorrecta: false },
+            { id: 'Normas económicas.', texto: '', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -56,10 +73,27 @@ export default {
           imagen: require('@/assets/actividad/3.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: false },
-            { id: 'b', texto: '', esCorrecta: false },
-            { id: 'c', texto: '', esCorrecta: true },
-            { id: 'd', texto: '', esCorrecta: false },
+            {
+              id: 'Solo las relaciones externas de la empresa.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id: 'Las relaciones entre la empresa y el Estado.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id:
+                'El funcionamiento interno y el comportamiento del capital humano.',
+              texto: '',
+              esCorrecta: true,
+            },
+            {
+              id: 'Las actividades comerciales internacionales.',
+              texto: '',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -71,10 +105,27 @@ export default {
           imagen: require('@/assets/actividad/4.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: false },
-            { id: 'b', texto: '', esCorrecta: false },
-            { id: 'c', texto: '', esCorrecta: true },
-            { id: 'd', texto: '', esCorrecta: false },
+            {
+              id: 'Aumenta los impuestos empresariales.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id: 'Limita la contratación de empleados.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id:
+                'Protege al empresario y a la empresa ante faltas cometidas por los empleados.',
+              texto: '',
+              esCorrecta: true,
+            },
+            {
+              id: 'Genera conflictos interpersonales.',
+              texto: '',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -86,10 +137,10 @@ export default {
           imagen: require('@/assets/actividad/1.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: false },
-            { id: 'b', texto: '', esCorrecta: false },
-            { id: 'c', texto: '', esCorrecta: true },
-            { id: 'd', texto: '', esCorrecta: false },
+            { id: 'Normas de convivencia.', texto: '', esCorrecta: false },
+            { id: 'Normas de vestimenta.', texto: '', esCorrecta: false },
+            { id: 'Normas de seguridad.', texto: '', esCorrecta: true },
+            { id: 'Normas de protocolo.', texto: '', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -101,10 +152,23 @@ export default {
           imagen: require('@/assets/actividad/2.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: true },
-            { id: 'b', texto: '', esCorrecta: false },
-            { id: 'c', texto: '', esCorrecta: false },
-            { id: 'd', texto: '', esCorrecta: false },
+            {
+              id:
+                'Un acuerdo para prestar un servicio público domiciliario según condiciones pactadas.',
+              texto: '',
+              esCorrecta: true,
+            },
+            {
+              id: 'Un contrato entre empleados y empleadores.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id: 'Un documento para regular la vestimenta de los empleados.',
+              texto: '',
+              esCorrecta: false,
+            },
+            { id: 'Una norma ecológica.', texto: '', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -116,10 +180,18 @@ export default {
           imagen: require('@/assets/actividad/3.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: true },
-            { id: 'b', texto: '', esCorrecta: false },
-            { id: 'c', texto: '', esCorrecta: false },
-            { id: 'd', texto: '', esCorrecta: false },
+            { id: 'Sanciones monetarias.', texto: '', esCorrecta: true },
+            { id: 'Reducción de costos.', texto: '', esCorrecta: false },
+            {
+              id: 'Acceso a mercados de capital.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id: 'Mejora en las condiciones laborales.',
+              texto: '',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -131,10 +203,27 @@ export default {
           imagen: require('@/assets/actividad/4.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: false },
-            { id: 'b', texto: '', esCorrecta: true },
-            { id: 'c', texto: '', esCorrecta: false },
-            { id: 'd', texto: '', esCorrecta: false },
+            {
+              id: 'Regular las relaciones laborales internacionales.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id:
+                'Diseñar e implementar medidas para mantener el bienestar físico, mental y social de los empleados.',
+              texto: '',
+              esCorrecta: true,
+            },
+            {
+              id: 'Controlar el uso de datos personales.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id: 'Establecer normas de vestimenta en el trabajo.',
+              texto: '',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -146,10 +235,14 @@ export default {
           imagen: require('@/assets/actividad/1.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: false },
-            { id: 'b', texto: '', esCorrecta: true },
-            { id: 'c', texto: '', esCorrecta: false },
-            { id: 'd', texto: '', esCorrecta: false },
+            { id: 'Normas de archivo.', texto: '', esCorrecta: false },
+            {
+              id: 'Normas de contratación externa.',
+              texto: '',
+              esCorrecta: true,
+            },
+            { id: 'Normas de jerarquía.', texto: '', esCorrecta: false },
+            { id: 'Normas de salubridad.', texto: '', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -160,10 +253,27 @@ export default {
           imagen: require('@/assets/actividad/2.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: '', esCorrecta: false },
-            { id: 'b', texto: '', esCorrecta: true },
-            { id: 'c', texto: '', esCorrecta: false },
-            { id: 'd', texto: '', esCorrecta: false },
+            {
+              id: 'Las relaciones entre empleados.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id:
+                'Las leyes y regulaciones del Estado aplicables a la empresa.',
+              texto: '',
+              esCorrecta: true,
+            },
+            {
+              id: 'Las actividades de ocio de los empleados.',
+              texto: '',
+              esCorrecta: false,
+            },
+            {
+              id: 'La comunicación interna de la empresa.',
+              texto: '',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
